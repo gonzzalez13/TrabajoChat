68af75898093a05c4e38e57c0a60459fa9f5c08d
Usuario <Usuario@DESKTOP-5U9J2GL>
2021-01-28 19:03:28 +0100

    cambio en main de la interfaces para el tamaño

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("ChatTefa(chat).fxml"));
-			Scene scene = new Scene(root,300,400);
+			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
